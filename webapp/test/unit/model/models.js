8f299655a75219ebca588bd4a08c896d8b1db400
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-11-20 13:45:46 -0500

    Reformatting to fit recommendations

--- webapp/test/unit/model/models.js
+++ webapp/test/unit/model/models.js
@@ -2,62 +2,49 @@
 
 sap.ui.define([
 	"sap/ui/demo/cart/model/models"
-], function (models) {
+], function (
+	models
+) {
 	"use strict";
 
 	QUnit.module("createDeviceModel", {
 		afterEach : function () {
 			this.oDeviceModel.destroy();
 		}
+	}, function () {
+		function isPhoneTestCase(assert, bIsPhone) {
+			this.stub(sap.ui.Device, "system", { phone : bIsPhone });
+			this.oDeviceModel = models.createDeviceModel();
+
+			assert.strictEqual(this.oDeviceModel.getProperty("/system/phone"), bIsPhone, "IsPhone property is correct");
+		}
+
+		QUnit.test("Should initialize a device model for desktop", function (assert) {
+			isPhoneTestCase.call(this, assert, false);
+		});
+
+		QUnit.test("Should initialize a device model for phone", function (assert) {
+			isPhoneTestCase.call(this, assert, true);
+		});
+
+		function isTouchTestCase(assert, bIsTouch) {
+			this.stub(sap.ui.Device, "support", { touch : bIsTouch });
+			this.oDeviceModel = models.createDeviceModel();
+
+			assert.strictEqual(this.oDeviceModel.getProperty("/support/touch"), bIsTouch, "IsTouch property is correct");
+		}
+
+		QUnit.test("Should initialize a device model for non touch devices", function (assert) {
+			isTouchTestCase.call(this, assert, false);
+		});
+
+		QUnit.test("Should initialize a device model for touch devices", function (assert) {
+			isTouchTestCase.call(this, assert, true);
+		});
+
+		QUnit.test("The binding mode of the device model should be one way", function (assert) {
+			this.oDeviceModel = models.createDeviceModel();
+			assert.strictEqual(this.oDeviceModel.getDefaultBindingMode(), "OneWay", "Binding mode is correct");
+		});
 	});
-
-	function isPhoneTestCase(assert, bIsPhone) {
-		// Arrange
-		this.stub(sap.ui.Device, "system", { phone : bIsPhone });
-
-		// System under test
-		this.oDeviceModel = models.createDeviceModel();
-
-		// Assert
-
-		assert.strictEqual(this.oDeviceModel.getProperty("/system/phone"), bIsPhone, "IsPhone property is correct");
-	}
-
-	QUnit.test("Should initialize a device model for desktop", function (assert) {
-		isPhoneTestCase.call(this, assert, false);
-	});
-
-	QUnit.test("Should initialize a device model for phone", function (assert) {
-		isPhoneTestCase.call(this, assert, true);
-	});
-
-	function isTouchTestCase(assert, bIsTouch) {
-		// Arrange
-		this.stub(sap.ui.Device, "support", { touch : bIsTouch });
-
-		// System under test
-		this.oDeviceModel = models.createDeviceModel();
-
-		// Assert
-		assert.strictEqual(this.oDeviceModel.getProperty("/support/touch"), bIsTouch, "IsTouch property is correct");
-	}
-
-	QUnit.test("Should initialize a device model for non touch devices", function (assert) {
-		isTouchTestCase.call(this, assert, false);
-	});
-
-	QUnit.test("Should initialize a device model for touch devices", function (assert) {
-		isTouchTestCase.call(this, assert, true);
-	});
-
-	QUnit.test("The binding mode of the device model should be one way", function (assert) {
-
-		// System under test
-		this.oDeviceModel = models.createDeviceModel();
-
-		// Assert
-		assert.strictEqual(this.oDeviceModel.getDefaultBindingMode(), "OneWay", "Binding mode is correct");
-	});
-
-
 });
