1748f814c18fb68683074664ac2fcc8daaff2616
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-11-20 13:45:54 -0500

    Reformatting to fit recommendations

--- webapp/test/unit/model/LocalStorageModel.js
+++ webapp/test/unit/model/LocalStorageModel.js
@@ -3,21 +3,22 @@
 sap.ui.define([
 	"sap/ui/demo/cart/model/LocalStorageModel",
 	"sap/ui/util/Storage"
-], function (LocalStorageModel, Storage) {
+], function (
+	LocalStorageModel,
+	Storage
+) {
 	"use strict";
 
-	QUnit.module("LocalStorageModel - Defaults");
+	QUnit.module("LocalStorageModel - Defaults", function () {
+		QUnit.test("Should intialize the local storage model with its defaults", function (assert) {
+			var oLocalStorageModel = new LocalStorageModel();
 
-	QUnit.test("Should intialize the local storage model with its defaults", function (assert) {
-		// act
-		var oLocalStorageModel = new LocalStorageModel();
+			assert.strictEqual(oLocalStorageModel._STORAGE_KEY, "LOCALSTORAGE_MODEL", "Default storage key was set");
 
-		// assert
-		assert.strictEqual(oLocalStorageModel._STORAGE_KEY, "LOCALSTORAGE_MODEL", "Default storage key was set");
-
-		// cleanup
-		oLocalStorageModel.destroy();
+			oLocalStorageModel.destroy();
+		});
 	});
+
 
 	QUnit.module("LocalStorageModel - Parameters", {
 		beforeEach: function() {
@@ -38,40 +39,35 @@ sap.ui.define([
 			this.fnGetStub.restore();
 			this.oLocalStorageModel.destroy();
 		}
-	});
+	}, function () {
+		QUnit.test("Should intialize the local storage model properly", function (assert) {
+			assert.ok(this.fnLoadData.calledOnce, " The data was read from the local storage by calling the constructor");
+			assert.deepEqual(this.oLocalStorageModel.getData(), {1: 2, 3: 4}, "The initial data was set on the model");
+			assert.strictEqual(this.oLocalStorageModel.iSizeLimit, 1000000, "The size limit on the model has been increased");
+		});
 
-	QUnit.test("Should intialize the local storage model properly", function (assert) {
-		assert.ok(this.fnLoadData.calledOnce, " The data was read from the local storage by calling the constructor");
-		assert.deepEqual(this.oLocalStorageModel.getData(), {1: 2, 3: 4}, "The initial data was set on the model");
-		assert.strictEqual(this.oLocalStorageModel.iSizeLimit, 1000000, "The size limit on the model has been increased");
-	});
+		QUnit.test("Should read the data from the local storage", function (assert) {
+			this.oLocalStorageModel._loadData();
+			assert.ok(this.fnGetStub.calledOnce, "The data was read from the local storage");
+			assert.ok(this.fnGetStub.calledWith("MY_KEY"), "The custom storage key was used");
+		});
 
-	QUnit.test("Should read the data from the local storage", function (assert) {
-		// act
-		this.oLocalStorageModel._loadData();
-		//Assert
-		assert.ok(this.fnGetStub.calledOnce, "The data was read from the local storage");
-		assert.ok(this.fnGetStub.calledWith("MY_KEY"), "The custom storage key was used");
-	});
+		QUnit.test("Should write the data to the local storage when calling \"setProperty\" on the model", function (assert) {
+			this.oLocalStorageModel.setProperty("/foo", "bar");
+			assert.ok(this.fnPutStub.calledOnce, "The data was written to the local storage");
+			assert.ok(this.fnPutStub.calledWith("MY_KEY"), "The custom storage key was used");
+		});
 
-	QUnit.test("Should write the data to the local storage when calling \"setProperty\" on the model", function (assert) {
-		//Assert
-		this.oLocalStorageModel.setProperty("/foo", "bar");
-		assert.ok(this.fnPutStub.calledOnce, "The data was written to the local storage");
-		assert.ok(this.fnPutStub.calledWith("MY_KEY"), "The custom storage key was used");
-	});
+		QUnit.test("Should write the data to the local storage when calling \"setData\" on the model", function (assert) {
+			this.oLocalStorageModel.setData({foo:"bar"});
+			assert.ok(this.fnPutStub.calledOnce, "The data was written to the local storage");
+			assert.ok(this.fnPutStub.calledWith("MY_KEY"), "The custom storage key was used");
+		});
 
-	QUnit.test("Should write the data to the local storage when calling \"setData\" on the model", function (assert) {
-		//Assert
-		this.oLocalStorageModel.setData({foo:"bar"});
-		assert.ok(this.fnPutStub.calledOnce, "The data was written to the local storage");
-		assert.ok(this.fnPutStub.calledWith("MY_KEY"), "The custom storage key was used");
-	});
-
-	QUnit.test("Should write the data to the local storage when calling \"refresh\" on the model", function (assert) {
-		//Assert
-		this.oLocalStorageModel.refresh(true);
-		assert.ok(this.fnPutStub.calledOnce, "The data was written to the local storage");
-		assert.ok(this.fnPutStub.calledWith("MY_KEY"), "The custom storage key was used");
+		QUnit.test("Should write the data to the local storage when calling \"refresh\" on the model", function (assert) {
+			this.oLocalStorageModel.refresh(true);
+			assert.ok(this.fnPutStub.calledOnce, "The data was written to the local storage");
+			assert.ok(this.fnPutStub.calledWith("MY_KEY"), "The custom storage key was used");
+		});
 	});
 });
