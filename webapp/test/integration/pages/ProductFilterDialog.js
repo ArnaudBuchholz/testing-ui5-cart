356b347a2dbeb10a5393ffb6b29d55bef1a16a9c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-11-26 17:06:18 -0500

    Separate this page to make journeys more clear

---
+++ webapp/test/integration/pages/ProductFilterDialog.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+	"sap/ui/test/Opa5",
+], function (
+	Opa5
+) {
+	"use strict";
+
+	Opa5.createPageObjects({
+		onTheProductFilterDialog: {
+			viewName: "Category",
+
+			actions: {
+			},
+
+			assertions: {
+			}
+		}
+	});
+});
