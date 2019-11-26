c5aaabcff09159cd335567efaa1b4a5c31735c54
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-11-26 17:03:01 -0500

    Put things in common

--- webapp/test/integration/AllJourneys.js
+++ webapp/test/integration/AllJourneys.js
@@ -1,23 +1,10 @@
 sap.ui.define([
-	"sap/ui/test/Opa5",
-	"Startup",
+	"./configureOpa",
+	// Journeys
 	"./WelcomeJourney",
 	"./NavigationJourney",
 	"./DeleteProductJourney",
 	"./BuyProductJourney",
 	"./FilterJourney",
 	"./ComparisonJourney"
-], function (Opa5, Startup) {
-	"use strict";
-
-	Opa5.extendConfig({
-		arrangements: new Startup(),
-		viewNamespace: "sap.ui.demo.cart.view.",
-		pollingInterval: 50,
-		autoWait: true,
-		appParams: {
-			"sap-ui-language": "EN",
-			"sap-ui-animation": false
-		}
-	});
-});
+]);
