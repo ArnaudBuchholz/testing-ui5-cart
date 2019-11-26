57e4370b3e76f3c37036d1d655cfc42ee136483e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-11-26 17:03:51 -0500

    Put things in common

--- webapp/test/integration/PhoneJourneys.js
+++ webapp/test/integration/PhoneJourneys.js
@@ -1,12 +1,4 @@
 sap.ui.define([
-	"sap/ui/test/Opa5",
-	"Startup",
+	"./configureOpa",
 	"./PhoneNavigationJourney"
-], function (Opa5, Startup) {
-	"use strict";
-	Opa5.extendConfig({
-		arrangements: new Startup(),
-		viewNamespace: "sap.ui.demo.cart.view.",
-		autoWait: true
-	});
-});
+]);
