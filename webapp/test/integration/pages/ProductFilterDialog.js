06705b1895ad1b389f7e2a5e18b84af229584427
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-11-28 17:34:40 -0500

    Reviewed

--- webapp/test/integration/pages/ProductFilterDialog.js
+++ webapp/test/integration/pages/ProductFilterDialog.js
@@ -1,215 +1,130 @@
 sap.ui.define([
 	"sap/ui/test/Opa5",
+	"sap/ui/test/matchers/PropertyStrictEquals",
+	"sap/ui/test/matchers/I18NText",
+	"sap/ui/test/actions/Press"
 ], function (
-	Opa5
+	Opa5,
+	PropertyStrictEquals,
+	I18NText,
+	Press
 ) {
 	"use strict";
 
 	Opa5.createPageObjects({
 		onTheProductFilterDialog: {
-			viewName: "Category",
-
 			actions: {
-				_iSelectTheFilteringOption: function (sOptionTitle, sOptionKey) {
+				_iPressTheFilterOption: function (sOptionTitle, sOptionKey) {
 					return this.waitFor({
+						searchOpenDialogs: true,
 						controlType: "sap.m.StandardListItem",
 						matchers: new I18NText({ propertyName: "title", key: sOptionKey }),
 						actions: new Press(),
 						success: function () {
-							Opa5.assert.ok(true, "The " + sOptionTitle + " filtering option was pressed");
+							Opa5.assert.ok(true, "The " + sOptionTitle + " was pressed");
 						},
-						errorMessage: "The " + sOptionTitle + " filtering option was not found and could not be pressed"
+						errorMessage: "The " + sOptionTitle + " was not found and could not be pressed"
 					});
 				},
 
-				iSelectTheAvailabilityFilteringOption: function () {
-					return this._iSelectTheFilteringOption("availability", "availabilityFilterTitle");
+				iPressTheAvailabilityFilteringOption: function () {
+					return this._iPressTheFilterOption("availability filtering option", "availabilityFilterTitle");
 				},
 
-				iSelectTheSupplierFilteringOption: function () {
-					return this._iSelectTheFilteringOption("supplier", "supplierFilterTitle");
+				iPressTheAvailableFilter: function () {
+					return this._iPressTheFilterOption("available check box", "availableFilterTitle");
 				},
 
-				iSelectTheAvailableFilter: function () {
-					return this._iSelectTheFilteringOption("available", "availableFilterTitle");
+				iPressTheOutOfStockFilter: function () {
+					return this._iPressTheFilterOption("out of stock check box", "outOfStockFilterTitle");
 				},
 
-				iSelectTheDiscontinuedFilter: function () {
-					return this._iSelectTheFilteringOption("discontinued", "discontinuedFilterTitle");
+				iPressTheDiscontinuedFilter: function () {
+					return this._iPressTheFilterOption("discontinued checkbox", "discontinuedFilterTitle");
 				},
 
-				iSelectTheTechnocomFilter: function () {
-					this.waitFor({
-						controlType: "sap.m.StandardListItem",
-						matchers: new PropertyStrictEquals({name: "title", value: "Technocom"}),
-						actions: new Press(),
-						errorMessage: "The Technocom check box was not found and could not be selected"
+				iPressThePriceFilteringOption: function () {
+					return this._iPressTheFilterOption("price filtering option", "priceFilterTitle");
+				},
+
+				iSetPriceFilterValues: function (iFrom, iTo) {
+					return this.waitFor({
+						searchOpenDialogs: true,
+						controlType: "sap.m.RangeSlider",
+						success: function (aSliders) {
+							var oSlider = aSliders[0];
+							oSlider.setValue(iFrom).setValue2(iTo);
+							// The slider change event is not fired automatically and need to be manually fired
+							oSlider.fireEvent("change", {range: oSlider.getRange()});
+						},
+						success: function () {
+							Opa5.assert.ok(true, "The price slider control was set to " + iFrom + " - " + iTo);
+						},
+						errorMessage: "The price slider control was not displayed and could not be modified"
 					});
 				},
 
-				iSelectTheOutOfStockFilter: function () {
-					this.waitFor({
+				iPressTheSupplierFilteringOption: function () {
+					return this._iPressTheFilterOption("supplier filtering option", "supplierFilterTitle");
+				},
+
+				iPressTheSupplierFilter: function (sSupplierName) {
+					return this.waitFor({
+						searchOpenDialogs: true,
 						controlType: "sap.m.StandardListItem",
-						matchers: new PropertyStrictEquals({name: "title", value: "Out of Stock"}),
+						matchers: new PropertyStrictEquals({name: "title", value: sSupplierName}),
 						actions: new Press(),
-						errorMessage: "The out of stock check box was not found and could not be selected"
+						success: function () {
+							Opa5.assert.ok(true, "The supplier '" + sSupplierName + "' check box was pressed");
+						},
+						errorMessage: "The supplier '" + sSupplierName + "' check box was not found and could not be pressed"
 					});
 				},
 
-				iDeselectTheAvailableFilter: function () {
-					this.waitFor({
-						controlType: "sap.m.StandardListItem",
-						matchers: new PropertyStrictEquals({name: "title", value: "Available"}),
+				_iPressTheButton: function (sButtonLabel, sButtonId) {
+					return this.waitFor({
+						searchOpenDialogs: true,
+						controlType: "sap.m.Button",
+						id: new RegExp(sButtonId + "$"), // ends with
 						actions: new Press(),
-						errorMessage: "The available check box was not found and could not be deselected"
-					});
-				},
-
-				iDeselectTheDiscontinuedFilter: function () {
-					this.waitFor({
-						controlType: "sap.m.StandardListItem",
-						matchers: new PropertyStrictEquals({name: "title", value: "Discontinued"}),
-						actions: new Press(),
-						errorMessage: "The discontinued check box was not found and could not be deselected"
-					});
-				},
-
-				iDeselectTheTechnoComFilter: function () {
-					this.waitFor({
-						controlType: "sap.m.StandardListItem",
-						matchers: new PropertyStrictEquals({name: "title", value: "Technocom"}),
-						actions: new Press(),
-						errorMessage: "The Technocom check box was not found and could not be deselected"
+						success: function () {
+							Opa5.assert.ok(true, "The " + sButtonLabel + " button in the dialog was pressed");
+						},
+						errorMessage: "The " + sButtonLabel + " button in the dialog was not found and could not be pressed"
 					});
 				},
 
 				iPressOkButton: function () {
-					this.waitFor({
-						controlType: "sap.m.Button",
-						matchers: new PropertyStrictEquals({name: "text", value: "OK"}),
-						actions: new Press(),
-						errorMessage: "The ok button in the dialog was not found and could not be pressed"
-					});
+					return this._iPressTheButton("OK", "acceptbutton");
 				},
 
 				iPressCancelButton: function () {
-					this.waitFor({
-						controlType: "sap.m.Button",
-						matchers: new PropertyStrictEquals({name: "text", value: "Cancel"}),
-						actions: new Press(),
-						errorMessage: "The cancel button in the dialog was not found and could not be pressed"
-					});
+					return this._iPressTheButton("Cancel", "cancelbutton");
 				},
 
 				iPressResetButton: function () {
-					this.waitFor({
-						controlType: "sap.m.Button",
-						matchers: new PropertyStrictEquals({name: "text", value: "Reset"}),
-						actions: new Press(),
-						errorMessage: "The reset button in the dialog was not found and could not be pressed"
-					});
+					return this._iPressTheButton("Reset", "resetbutton");
 				},
 
-				iPressTheBackButtonInDialog: function () {
-					this.waitFor({
-						controlType: "sap.m.Button",
-						matchers: new PropertyStrictEquals({name: "icon", value: "sap-icon://nav-back"}),
-						actions: new Press(),
-						errorMessage: "The back button in the dialog was not found and could not be pressed"
-					});
-				},
-
-				iSelectThePriceFilteringOption: function () {
-					this.waitFor({
-						controlType: "sap.m.StandardListItem",
-						matchers: new PropertyStrictEquals({name: "title", value: "Price"}),
-						actions: new Press(),
-						errorMessage: "The price filtering option was not found and could not be pressed"
-					});
-				},
-				iSetPriceFilterValues: function () {
-					this.waitFor({
-						controlType: "sap.m.RangeSlider",
-						matchers: new PropertyStrictEquals({name: "value2", value: 5000}),
-						success: function (oSlider) {
-							oSlider[0].setValue(200).setValue2(500);
-						},
-						errorMessage: "The range slider control was not displayed and could not be scrolled"
-
-					});
-				},
-
-				iChangeThePriceFilterValues: function () {
-					this.waitFor({
-						controlType: "sap.m.RangeSlider",
-						matchers: new PropertyStrictEquals({name: "value2", value: 500}),
-						success: function (oSlider) {
-							oSlider[0].setValue(500).setValue2(1000);
-						},
-						errorMessage: "The range slider control was not displayed and could not be scrolled"
-
-					});
-				},
-				iChangeToTheDefaultPriceFilterValues: function () {
-					this.waitFor({
-						controlType: "sap.m.RangeSlider",
-						matchers: new PropertyStrictEquals({name: "value2", value: 500}),
-						success: function (oSlider) {
-							oSlider[0].setValue(0).setValue2(5000);
-							// the slider change event is not fired automatically and need to be manually fired
-							oSlider[0].fireEvent("change", {range: oSlider[0].getRange()});
-						},
-						errorMessage: "The range slider control was not displayed and could not be scrolled"
-
-					});
-				},
-
-				iFilterOnAvailability: function () {
-					this.iPressTheFilterButton();
-					this.iSelectTheAvailabilityFilteringOption();
-					this.iSelectTheAvailableFilter();
-					this.iSelectTheDiscontinuedFilter();
-					this.iPressOkButton();
-				},
-				iFilterOnSupplier: function () {
-					this.iPressTheFilterButton();
-					this.iSelectTheSupplierFilteringOption();
-					this.iSelectTheTechnocomFilter();
-					this.iPressOkButton();
-				},
-				iFilterOnAvailabilityAndPrice: function () {
-					this.iPressTheFilterButton();
-					this.iSelectTheOutOfStockFilter();
-					this.iPressTheBackButtonInDialog();
-					this.iSelectThePriceFilteringOption();
-					this.iSetPriceFilterValues();
-					this.iPressOkButton();
-				},
-				iCancelAPriceFilterChange: function () {
-					this.iPressTheFilterButton();
-					this.iChangeThePriceFilterValues();
-					this.iPressCancelButton();
-				},
-				iChangeToTheDefaultFilterPriceValues: function () {
-					this.iSelectThePriceFilteringOption();
-					this.iChangeToTheDefaultPriceFilterValues();
-					this.iPressOkButton();
-				},
-				iRemoveTheAvailabilityFilters: function () {
-					this.iPressTheFilterButton();
-					this.iDeselectTheAvailableFilter();
-					this.iDeselectTheDiscontinuedFilter();
-					this.iPressOkButton();
-				},
-				iRemoveTheSupplierFilter: function () {
-					this.iPressTheFilterButton();
-					this.iDeselectTheTechnoComFilter();
-					this.iPressOkButton();
+				iPressTheBackButton: function () {
+					return this._iPressTheButton("Back", "backbutton");
 				}
-
 			},
-
 			assertions: {
+				iShouldSeeThePriceFilterCount: function (iCountNumber) {
+					return this.waitFor({
+						searchOpenDialogs: true,
+						controlType: "sap.m.StandardListItem",
+						matchers: [
+							new I18NText({ propertyName: "title", key: "priceFilterTitle" }),
+							new PropertyStrictEquals({name: "counter", value: iCountNumber})
+						],
+						success: function() {
+							Opa5.assert.ok(true, sSuccessMessage, "The price filter count is correctly set up");
+						},
+						errorMessage: "The price filter count isn't correctly set up"
+					});
+				}
 			}
 		}
 	});
