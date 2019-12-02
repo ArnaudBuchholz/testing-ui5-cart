5dc361a1c11f6f9eedf36b2723512451c59fcc24
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-12-01 23:38:52 -0500

    Reviewed

--- webapp/test/integration/DeleteProductJourney.js
+++ webapp/test/integration/DeleteProductJourney.js
@@ -8,14 +8,12 @@ sap.ui.define([
 	opaTest("Should see the product list", function (Given, When, Then) {
 		// Arrangements
 		Given.iStartMyApp();
-
 		// Actions
-		When.onHome.iPressOnTheFlatScreensCategory();
-
+		When.onTheCategoryList.iPressOnTheFlatScreensCategory();
 		// Assertions
-		Then.onTheCategoryProductList.iShouldBeTakenToTheCategory("Flat Screens").
-			and.iShouldSeeTheProductList().
-			and.iShouldSeeSomeEntriesInTheProductList();
+		Then.onTheCategoryProductList.iShouldBeTakenToTheCategory("Flat Screens")
+			.and.iShouldSeeTheProductList()
+			.and.iShouldSeeSomeEntriesInTheProductList();
 	});
 
 	opaTest("Should add a product to the cart and enable the edit button", function (Given, When, Then) {
@@ -23,17 +21,15 @@ sap.ui.define([
 		When.onTheCategoryProductList.iPressOnTheFirstProduct();
 		When.onTheProduct.iAddTheDisplayedProductToTheCart();
 		When.onTheProduct.iToggleTheCart();
-
 		// Assertions
-		Then.onTheCart.iShouldSeeSomeProductsInMyCart().
-		and.iShouldSeeTheEditButtonEnabled().
-		and.iShouldSeeTheProceedButtonEnabled();
+		Then.onTheCart.iShouldSeeSomeProductsInMyCart()
+			.and.iShouldSeeTheEditButtonEnabled()
+			.and.iShouldSeeTheProceedButtonEnabled();
 	});
 
 	opaTest("Should see the delete button after pressing the edit button", function (Given, When, Then) {
 		// Actions
 		When.onTheCart.iPressOnTheEditButton();
-
 		// Assertions
 		Then.onTheCart.iShouldSeeTheDeleteButton(0);
 	});
@@ -41,7 +37,6 @@ sap.ui.define([
 	opaTest("Should see the confirmation dialog", function (Given, When, Then) {
 		// Actions
 		When.onTheCart.iPressOnTheDeleteButtonOfItem(0);
-
 		// Assertions
 		Then.onTheDialog.iShouldBeTakenToTheConfirmationDialog();
 	});
@@ -49,7 +44,6 @@ sap.ui.define([
 	opaTest("Should cancel the delete process", function (Given, When, Then) {
 		// Actions
 		When.onTheDialog.iPressCancelOnTheConfirmationDialog();
-
 		// Assertions
 		Then.onTheCart.iShouldBeTakenToTheCart();
 	});
@@ -57,27 +51,26 @@ sap.ui.define([
 	opaTest("Should see the edit button", function (Given, When, Then) {
 		// Actions
 		When.onTheCart.iPressOnTheSaveChangesButton();
-
 		// Assertions
 		Then.onTheCart.iShouldSeeTheEditButtonEnabled();
 	});
 
 	opaTest("Should delete the product from the cart", function (Given, When, Then) {
 		// Actions
-		When.onTheCart.iPressOnTheEditButton().and.iPressOnTheDeleteButtonOfItem(0);
+		When.onTheCart.iPressOnTheEditButton()
+			.and.iPressOnTheDeleteButtonOfItem(0);
 		When.onTheDialog.iPressDeleteButtonOnTheConfirmationDialog();
-
 		// Assertions
-		Then.onTheCart.iShouldNotSeeTheDeletedProductInTheCart("HT-1254" /* Bending Screen 21HD */).
-			and.iShouldSeeTheTotalPriceEqualToZero();
+		Then.onTheCart.iShouldNotSeeTheDeletedProductInTheCart("HT-1254" /* Bending Screen 21HD */)
+			.and.iShouldSeeTheTotalPriceEqualToZero();
 	});
 
 	opaTest("Edit button should be disabled", function (Given, When, Then) {
 		// Actions
 		When.onTheCart.iPressOnTheSaveChangesButton();
 		// Assertions
-		Then.onTheCart.iShouldSeeTheEditButtonDisabled().
-		and.iShouldSeeTheProceedButtonDisabled();
+		Then.onTheCart.iShouldSeeTheEditButtonDisabled()
+			.and.iShouldSeeTheProceedButtonDisabled();
 		// Cleanup
 		Then.iTeardownMyApp();
 	});
